b5d125bacb86e86789b185f946a349911b4462a9
cmaalik97 <cmaalik97@gmail.com>
2026-04-04 11:44:13 -0700

    Exercise 3

--- AnswersExercise/src/App.jsx
+++ AnswersExercise/src/App.jsx
@@ -1,10 +1,17 @@
 // import UserCard from "./Dugsiye/Exercise1/UserCard";
-import Blog from "./Dugsiye/Exercise2/Blog";
+// import Blog from "./Dugsiye/Exercise2/Blog";
+import UserCardDaynamic from "./Dugsiye/Exercise3/UserCardDaynamic";
 function App(){
+  
   return <>
   {/* <UserCard/> */}
-  <Blog/>
-
+  {/* <Blog/> */}
+  
+  <UserCardDaynamic name="Abdimalik" email="cmaalik97@gmail.com"/>
+  <UserCardDaynamic name="Faadumo " email="faay65@gmial.com"/>
+  <UserCardDaynamic name="Maryama " email="marymoxaaji5@gmial.com"/>
+  <UserCardDaynamic name="Faysal geedi " email="faysald09@gmial.com"/>
+  <UserCardDaynamic/>
   </>
 }
 export default App;
---
+++ AnswersExercise/src/Dugsiye/Exercise3/UserCardDaynamic.jsx
@@ -0,0 +1,8 @@
+function UserCardDaynamic({name="UNKNOW" , email="Unknow"}){
+    return <div>
+        <h1>Name: {name}</h1>
+        <p>Email:{email}</p>
+        <h1>--------------------</h1>
+    </div>
+}
+export default UserCardDaynamic;
